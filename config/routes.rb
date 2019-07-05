4cf7b6a89dec4dd8f940303b2830e7749e8bf168
voycik <voycik@gmail.com>
2019-07-05 15:39:08 +0200

    Generate appointments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'appointments/index'
+  get 'appointments/show'
+  get 'appointments/new'
+  get 'appointments/create'
+  get 'appointments/edit'
+  get 'appointments/update'
+  get 'appointments/destroy'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
