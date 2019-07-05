d5949229ff0927ddc4f1ec3ee05c6b772b7486d1
voycik <voycik@gmail.com>
2019-07-05 15:45:21 +0200

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'appointments/index'
-  get 'appointments/show'
-  get 'appointments/new'
-  get 'appointments/create'
-  get 'appointments/edit'
-  get 'appointments/update'
-  get 'appointments/destroy'
+  resources :appointments
+  root 'appointments#index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
