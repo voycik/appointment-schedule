2eefcab322d5e7589b8b4b9afc3e55da6a5c086c
voycik <voycik@gmail.com>
2019-09-06 22:58:28 +0200

    Appointment model presence validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Appointment < ApplicationRecord
+  validates :user_id, :patient_card_id, :start_time, :end_time, presence: true
   belongs_to :user
   belongs_to :patient_card
 end
