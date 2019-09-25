865ca41f6f5ec920693d5b70821b8fb072eec3fe
voycik <voycik@gmail.com>
2019-09-25 17:11:30 +0200

    First appoinment controller index spec

--- spec/controllers/appointments_controller_spec.rb
+++ spec/controllers/appointments_controller_spec.rb
@@ -2,10 +2,28 @@ require 'rails_helper'
 
 RSpec.describe AppointmentsController, type: :controller do
 
+  let!(:physio) { FactoryBot.create(:physio) }
+  let!(:patient_card) { FactoryBot.create(:patient_card) }
+
+  it 'has a valid factory' do
+    expect(FactoryBot.create(:appointment)).to be_valid
+  end
+
+  let!(:physio) { FactoryBot.create(:physio) }
+  let!(:patient_card) { FactoryBot.create(:patient_card) }
+  let(:appointment) { FactoryBot.create(:appointment) }
+
+  subject { appointment }
   describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+
+    context 'Signed in physioterapist' do
+      before :each do
+        sign_in physio
+        get :index
+      end
+      it 'renders the :index view' do
+       expect(response).to render_template(:index)
+     end
     end
   end
   #
@@ -16,19 +34,19 @@ RSpec.describe AppointmentsController, type: :controller do
   #   end
   # end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #new" do
+  #   it "returns http success" do
+  #     get :new
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET #create" do
+  #   it "returns http success" do
+  #     get :create
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
   # describe "GET #edit" do
   #   it "returns http success" do
