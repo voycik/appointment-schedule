45f65321b33699ff28c0dab57923980403e7b422
voycik <voycik@gmail.com>
2019-09-28 15:30:58 +0200

    Appointments controller :create spec

--- spec/controllers/appointments_controller_spec.rb
+++ spec/controllers/appointments_controller_spec.rb
@@ -96,32 +96,56 @@ RSpec.describe AppointmentsController, type: :controller do
       end
     end
   end
-  #
-  # describe "GET #create" do
-  #   it "returns http success" do
-  #     get :create
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
 
-  # describe "GET #edit" do
-  #   it "returns http success" do
-  #     get :edit
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe 'POST #create' do
+    context 'Signed in physioterapist' do
+      before :each do
+        sign_in physio
+      end
+      context 'with valid attributes' do
+        it 'creates new appointment' do
+          expect do
+            post :create, params: { appointment: {
+              user_id: physio.id,
+              patient_card_id: patient_card.id,
+              start_time: Time.now + 48.hours,
+              end_time: Time.now + 49.hours
+            } }
+          end .to change { Appointment.count }.by(1)
+        end
 
-  # describe "GET #update" do
-  #   it "returns http success" do
-  #     get :update
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-  #
-  # describe "GET #destroy" do
-  #   it "returns http success" do
-  #     get :destroy
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+        it 'redirects to the new appointment' do
+          post :create, params: { appointment: {
+            user_id: physio.id,
+            patient_card_id: patient_card.id,
+            start_time: Time.now + 48.hours,
+            end_time: Time.now + 49.hours
+          } }
+          expect(response).to redirect_to appointments_path
+        end
+      end
+      context 'with invalid attributes' do
+        it 'not create new appointment' do
+          expect do
+            post :create, params: { appointment: {
+              user_id: nil,
+              patient_card_id: nil,
+              start_time: nil,
+              end_time: nil
+            } }
+          end .to_not change { Appointment.count }
+        end
+
+        it 're-renders the new method' do
+          post :create, params: { appointment: {
+            user_id: nil,
+            patient_card_id: nil,
+            start_time: nil,
+            end_time: nil
+          } }
+          expect(response).to render_template :new
+        end
+      end
+    end
+  end
 end
