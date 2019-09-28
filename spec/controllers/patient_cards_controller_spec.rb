9e5c0cbb8f09f02acaf564f3f2eeefacd5e15191
voycik <voycik@gmail.com>
2019-09-28 16:08:35 +0200

    Patient cards controller spec

--- spec/controllers/patient_cards_controller_spec.rb
+++ spec/controllers/patient_cards_controller_spec.rb
@@ -1,54 +1,172 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe PatientCardsController, type: :controller do
+  it 'has a valid factory' do
+    expect(FactoryBot.create(:appointment)).to be_valid
+  end
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  let!(:physio) { FactoryBot.create(:physio) }
+  let!(:patient_card) { FactoryBot.create(:patient_card) }
+  let(:appointment) { FactoryBot.create(:appointment) }
+
+  subject { patient_card }
+  describe 'GET #index' do
+    context 'Signed in physioterapist' do
+      before :each do
+        sign_in physio
+        get :index
+      end
+
+      it 'renders the :index view' do
+        expect(response).to render_template(:index)
+      end
+
+      it 'populates an array of patient cards' do
+        expect(assigns(:patients)).to eq([patient_card])
+      end
+    end
+
+    context 'Non-signed in physioterapist' do
+      before do
+        get :index
+      end
+
+      it 'redirects to sign in view' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
-  #
-  # describe "GET #show" do
-  #   it "returns http success" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
+  describe 'GET #show' do
+    context 'Signed in physioterapist' do
+      before :each do
+        sign_in physio
+        get :show, params: { id: patient_card.id }
+      end
+      it 'returns http success' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'renders the :show view' do
+        expect(response).to render_template(:show)
+      end
+    end
+    context 'Non-signed in physioterapist' do
+      before do
+        get :show, params: { id: patient_card.id }
+      end
+
+      it 'redirects to sign in view' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
-  #
-  # describe "GET #create" do
-  #   it "returns http success" do
-  #     get :create
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-  #
-  # describe "GET #edit" do
-  #   it "returns http success" do
-  #     get :edit
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-  #
-  # describe "GET #update" do
-  #   it "returns http success" do
-  #     get :update
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-  #
-  # describe "GET #destroy" do
-  #   it "returns http success" do
-  #     get :destroy
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
 
+  describe 'GET #new' do
+    context 'Signed in physioterapist' do
+      before :each do
+        sign_in physio
+        get :new
+      end
+      it 'returns http success' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'renders the :new view' do
+        expect(response).to render_template(:new)
+      end
+    end
+
+    context 'Non-signed in physioterapist' do
+      before do
+        get :new
+      end
+
+      it 'redirects to sign in view' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    context 'Signed in physioterapist' do
+      before :each do
+        sign_in physio
+      end
+      context 'with valid attributes' do
+        it 'creates new patient card' do
+          expect do
+            post :create, params: { patient_card: {
+              first_name: 'Jan',
+              last_name: 'Nowak',
+              date_of_birth: Time.now - 48.years,
+              phone_number: 123_123_123,
+              comments: 'Lorem ipsum'
+            } }
+          end .to change { PatientCard.count }.by(1)
+        end
+
+        it 'redirects to the new patient card' do
+          post :create, params: { patient_card: {
+            first_name: 'Jan',
+            last_name: 'Nowak',
+            date_of_birth: Time.now - 48.years,
+            phone_number: 123_123_123,
+            comments: 'Lorem ipsum'
+          } }
+          expect(response).to redirect_to patient_cards_path
+        end
+      end
+      context 'with invalid attributes' do
+        it 'not create new patient card' do
+          expect do
+            post :create, params: { patient_card: {
+              first_name: nil,
+              last_name: nil,
+              date_of_birth: nil,
+              phone_number: 'invalid number',
+              comments: 'nil'
+            } }
+          end .to_not change { PatientCard.count }
+        end
+
+        it 're-renders the new method' do
+          post :create, params: { patient_card: {
+            first_name: nil,
+            last_name: nil,
+            date_of_birth: nil,
+            phone_number: 'invalid number',
+            comments: 'nil'
+          } }
+          expect(response).to render_template :new
+        end
+      end
+    end
+    context 'Non-signed in physioterapist' do
+      it 'not create new patient card' do
+        expect do
+          post :create, params: { patient_card: {
+            first_name: nil,
+            last_name: nil,
+            date_of_birth: nil,
+            phone_number: 'invalid number',
+            comments: 'nil'
+          } }
+        end .to_not change { PatientCard.count }
+      end
+
+      it 'redirects to sign in view' do
+        post :create, params: { patient_card: {
+          first_name: nil,
+          last_name: nil,
+          date_of_birth: nil,
+          phone_number: 'invalid number',
+          comments: 'nil'
+        } }
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
 end
